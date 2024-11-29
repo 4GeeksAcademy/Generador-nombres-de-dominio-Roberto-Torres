a4a9b4451017f70eb2337a56b4233de6d34b2d6a
Roberto Torres Martínez <roberxiri@gmail.com>
2024-11-29 11:24:18 +0000

    Generador de nombres de dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronombres = ["the", "your", "our"];
+let adjetivos = ["great", "big", "crazy", "dangerous", "gorgeous"];
+let sustantivos = ["cat", "dog", "hyena"];
+let extension = [".es", ".com", ".io", ".net"];
+
+for (let i = 0; i < pronombres.length; i++) {
+  for (let j = 0; j < adjetivos.length; j++) {
+    for (let k = 0; k < sustantivos.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        console.log(
+          pronombres[i] + adjetivos[j] + sustantivos[k] + extension[l]
+        );
+      }
+    }
+  }
+}
